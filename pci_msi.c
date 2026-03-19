6a00f0664a97267d72f2e94e76d0c11ff75728f6
kirill <kirillpanovitsyn@gmail.com>
2026-03-20 02:40:17 +0300

    add minpci_write, minpci_read
    add functions for read from bar and write to memory with bar address

--- pci_msi.c
+++ pci_msi.c
@@ -1,16 +1,45 @@
-struct PmsiState {
-	PCIDevice parent_dev;
+#define BAR_SPACE 0x0
 
+struct Minpci_State {
+	PCIDevice parent_dev;
 	MemoryRegion mmio;
+	
+	uint64_t bar0_space;	
 };
 
 
-static void pmsi_class_init(ObjectClass *class, const void *data) {
+
+static void minpci_write(void *opaque, hwaddr addr, uint64_t val, unsigned size) {
+	struct Minpci_State *mpci = opaque;
+	
+	if (addr == BAR_SPACE) {
+		mpci->bar0_space = val;
+		pr_info("WRITE");
+		return;	
+	}
+	
+	pr_info("INVALID ADDR");
+	return;
+}
+
+static uint64_t minpci_read(void *opaque, hwaddr, unsigned size) {
+	struct Minpci_State *mpci = opaque;
+	uint64_t val = ~0ULL;
+	
+	if (addr == BAR_SPACE) {
+		val = mpci->bar0_space;	
+	}
+
+	return val;
+}
+
+
+static void minpci_class_init(ObjectClass *class, const void *data) {
 	DeviceClass *dc = DEVICE_CLASS(class);
 	PCIDeviceClass *k = PCI_DEVICE_CLASS(class);
 
-	k->realize = pmsi_realize;
-	k->exit = pmsi_uninit;
+	k->realize = minpci_realize;
+	k->exit = minpci_uninit;
 	k->vendor_id = PCI_VENDOR_ID_STUB;
 	k->device_id = PCI_DEVICE_ID_STUB;
 	k->revision = 0x00;
@@ -18,3 +47,4 @@ static void pmsi_class_init(ObjectClass *class, const void *data) {
 	
 	set_bit(DEVICE_CATEGORY_MISC, dc->categories);
 }
+
